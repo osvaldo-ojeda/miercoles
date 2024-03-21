9b2c0f40fc007bfd563ffcde354d9f39bcb3a401
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-03-20 22:44:05 -0300

    clase 1, git config, instalación de node, var, let y const

---
+++ index.js
@@ -0,0 +1,30 @@
+// let nombre= "osvaldito"
+// console.log(`🚀 ~ nombre:`, nombre)
+// console.log(`🚀 ~ nombre:`, nombre)
+// console.log(`🚀 ~ nombre:`, "kuka")
+// console.log(`🚀 ~ nombre:`, "kuka")
+
+// variables
+// var
+console.log(`🚀 ~ nombre:`, nombre);
+var nombre
+console.log(`🚀 ~ nombre:`, nombre)
+var nombre = "Osvaldo";
+nombre = "kuka";
+console.log(`🚀 ~ nombre:`, nombre);
+
+// let
+// console.log(`🚀 ~ apellido:`, apellido)
+let apellido
+apellido = "Gutierrez"
+// let apellido="ojeda"
+console.log(`🚀 ~ apellido:`, apellido)
+
+// const 
+// console.log(`🚀 ~ edad:`, edad)
+// const edad// esi no
+const  edad = 28;
+// edad=37
+// const edad =41
+console.log(`🚀 ~ edad:`, edad)
+

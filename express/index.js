5527c8fe373cc733eea80d529057614ea07a5857
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2024-05-08 20:33:15 -0300

    clase6

--- express/index.js
+++ express/index.js
@@ -1,20 +1,45 @@
-import express from "express"
-// console.log(`🚀 ~ express:`, express)
-const app=express()
-// console.log(`🚀 ~ app:`, app)
+import express from "express";
+import moment from "moment";
+import url from "url";
+import path from "path";
+// console.log(`🚀 ~ url:`, url)
+const app = express();
+let count = 0;
 
-app.get("/ok", (peticion, respuesta)=>{
-     respuesta.send("<h1>get ok</h1>")
-})
+// app.use("/archivo", express.static("public"));
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.post("/", (req, res)=>{
-     res.send("post ok")
-})
+console.log(`🚀 ~ import.meta.url:`, import.meta.url);
+console.log(`🚀 ~ __filename:`, __filename);
+console.log(`🚀 ~ __dirname:`, __dirname);
+app.get("/:archivo", (req, res) => {
+//   console.log(`🚀 ~ app.get ~ req:`, req)
+  const {archivo}= req.params
+  res.sendFile(path.join(__dirname, "public", `${archivo}.html`));
+});
 
+app.get("/ok", (peticion, respuesta) => {
+  respuesta.send("<h1>get ok</h1>");
+});
+app.post("/ok", (peticion, respuesta) => {
+  respuesta.send("<h1>post ok</h1>");
+});
 
+app.post("/", (req, res) => {
+  res.send("post");
+});
+app.get("/fecha", (req, res) => {
+  //   const date = new Date();
+  const date = moment().format("MMMM Do YYYY, h:mm:ss a");
+  res.send({ date });
+});
+app.get("/count", (req, res) => {
+  count++;
+  res.send({ count });
+});
 
-
-app.listen(8080,()=>{
-     console.log("servidor ok")
-})
+app.listen(8080, () => {
+  console.log("servidor ok");
+});
--- http/index.js
+++ http/index.js
@@ -1,11 +1,14 @@
 import { createServer } from "node:http";
 
 const server = createServer((peticion, respuesta) => {
-     console.log(`🚀 ~ server ~ peticion:`, peticion)
-     // console.log(`🚀 ~ server ~ respuesta:`, respuesta)
-     respuesta.end("<h1>que onda</h1>")
-
-})
+  console.log(`🚀 ~ server ~ peticion:`, peticion);
+  // console.log(`🚀 ~ server ~ respuesta:`, respuesta)
+  if (peticion.method === "GET" && peticion.url === "/hola") {
+    respuesta.end("<h1>Hola</h1>");
+  } else {
+    respuesta.end("<h1>Que onda?</h1>");
+  }
+});
 // console.log(`🚀 ~ server:`, server);
 
 server.listen(8080, () => {
